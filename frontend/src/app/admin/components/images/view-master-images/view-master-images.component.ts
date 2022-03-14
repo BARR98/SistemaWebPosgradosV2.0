61a414e61bf6fcfffcdba64dbe6e79beb43fe6b5
Brayan <brayanrodriguezrmz98@gmail.com>
2022-03-14 00:13:49 -0600

    Seccion de maestrias e imganes terminadas de forma responsive

--- frontend/src/app/admin/components/images/view-master-images/view-master-images.component.ts
+++ frontend/src/app/admin/components/images/view-master-images/view-master-images.component.ts
@@ -12,23 +12,40 @@ export class ViewMasterImagesComponent implements OnInit {
 
   master_name!: string;
   photos:any;
-
+  photos_msc:any;
+  size:any;
   constructor(private imageService : ImageService,
     private activateRoute :ActivatedRoute,
     private router : Router) { }
 
   ngOnInit(): void {
-
+    let tamanio:number = 0;
     this.activateRoute.params.subscribe(
       params => {
-        this.master_name = params['master']
+        this.master_name= params['master']
+
+        console.log(this.size)
       })
       this.imageService.getImages()
         .subscribe(res => {
         this.photos = res;
         console.log(this.photos);
+      
+        for (let index = 0; index < this.photos.length; index++) {
+          if (this.photos[index]['master'] == 'MSC') {
+            tamanio++;
+            console.log(tamanio)
+            this.size = tamanio
+            console.log(this.size)
+          }
+        
+        }
+
+        this.photos.length = tamanio+1
         
     })
+
+
   }
 
   deleteimage(id:string){
